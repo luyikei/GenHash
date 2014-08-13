8592c0a9eb5b86e7472f9d620091135922403893
luyikei <luyikei.qmltu@gmail.com>
2014-08-13 14:26:05 +0900

    bug fix

--- genhash.cpp
+++ genhash.cpp
@@ -14,7 +14,19 @@
 #include <kfiledialog.h>
 #include <QCryptographicHash>
 #include <QTextStream>
+#include "kdebug.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <fcntl.h>
+
+#include <limits.h>
 
 GenHash::GenHash( QObject* parent, const QVariantList & )
     : KParts::Plugin( parent )
@@ -23,28 +35,99 @@ GenHash::GenHash( QObject* parent, const QVariantList & )
     KAction *action = actionCollection()->addAction("generatehash");
     action->setText(i18n( "&Generate MD5 Hash..." ));
     connect(action, SIGNAL(triggered(bool)), SLOT(calcGenHash()));
+
+}
+
+void GenHash::sandbox_init()
+{
+    if (pipe(pipe_fd) < 0) {
+        perror("pipe");
+        exit(1);
+    }
+    if (pipe(pipe_result_fd) < 0) {
+        perror("pipe");
+        exit(1);
+    }
+
+    if ((child_pid = fork()) < 0) {
+        perror("fork");
+        exit(1);
+    }
+
 }
 
 void GenHash::calcGenHash()
 {
-    KParts::ReadOnlyPart * part = qobject_cast<KParts::ReadOnlyPart *>(parent());
+    sandbox_init();
 
-    QString basePath = KFileDialog::getOpenFileName(KUrl("kfiledialog:///konqueror"), i18n("*"), part->widget(), i18n("Open File To make MD5."));
+    if (child_pid){
 
-    if (basePath.size()) {
-        QFile file(basePath);
+        KParts::ReadOnlyPart * part = qobject_cast<KParts::ReadOnlyPart *>(parent());
+
+        QFile file(KFileDialog::getOpenFileName(KUrl("kfiledialog:///konqueror"), i18n("*"), part->widget(), i18n("Open File To make MD5.")));
 
         if (!file.open(QIODevice::ReadOnly))
         {
             return;
         }
 
-        QByteArray baseByteArray = file.readAll();
+        // TODO QFile
+        //char s[1024];
+        // write file content
+        {
+            close(pipe_fd[0]);
+            QByteArray s = file.readAll();
+            char *ch = s.data();
+            if (write(pipe_fd[1], ch, s.size()) < 0) {
+                perror("write");
+            }
+            close(pipe_fd[1]);
+        }
+        //read result
+        {
+            close(pipe_result_fd[1]);
+            char result[128];
+            if (read(pipe_result_fd[0], &result[0], 128) < 0){
+                perror("read");
+            }
+            close(pipe_result_fd[0]);
 
-        KMessageBox::information(part->widget(),i18n("Md5 : %1").arg(
-                                     QString(QCryptographicHash::hash(baseByteArray, QCryptographicHash::Md5).toHex())));
+            KMessageBox::information(part->widget(),i18n("Md5 : %1").arg(QString(QByteArray(result, 128))));
+        }
+    }else{
+        calcMD5();
+    }
+}
+
+void GenHash::calcMD5()
+{
+    QCryptographicHash hash(QCryptographicHash::Md5);
+
+    // read result;
+    {
+        char s[1024];
+        int count;
+        close(pipe_fd[1]);
+        while ((count = read(pipe_fd[0], &s[0], 1024)) > 0) {
+            hash.addData(s, count);
+        }
+        close(pipe_fd[0]);
     }
 
+    char* result = hash.result().toHex().data();
+
+    //write result
+    {
+
+        close(pipe_result_fd[0]);
+
+        write(pipe_result_fd[1], result, 128);
+
+        close(pipe_result_fd[1]);
+
+    }
+
+    exit(0);
 }
 
 K_PLUGIN_FACTORY(KonqGenHashFactory, registerPlugin<GenHash>();)
--- genhash.h
+++ genhash.h
@@ -2,6 +2,8 @@
 #define genhash_h
 
 #include <kparts/plugin.h>
+#include <QString>
+#include <QByteArray>
 
 class GenHash : public KParts::Plugin
 {
@@ -12,6 +14,20 @@ public:
 
 public slots:
     void calcGenHash();
+
+private:
+
+    void sandbox_init();
+    void readByteArryFromFile();
+
+    void calcMD5();
+
+    int pipe_fd[2];
+    int pipe_result_fd[2];
+    int child_pid;
+    int status;
+
+
 };
 
 #endif
